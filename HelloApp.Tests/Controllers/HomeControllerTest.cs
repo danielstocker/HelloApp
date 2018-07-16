66e7c2387bfc6252e0d8ae719197760d1fdc2c0c
Daniel Stocker <dasto@microsoft.com>
2018-07-16 11:40:14 +0100

    init

---
+++ HelloApp.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloApp;
+using HelloApp.Controllers;
+
+namespace HelloApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void HelloWithoutName()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Hello(null) as ViewResult;
+
+            // Assert
+            Assert.IsTrue(result.ViewBag.Message.StartsWith("Hello, please enter your name. Supply it as a GET parameter."));
+            Assert.IsFalse(result.ViewBag.Message.Contains("Bruce"));
+        }
+
+        [TestMethod]
+        public void HelloWithName()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Hello("Bruce") as ViewResult;
+
+            // Assert
+            Assert.IsTrue(result.ViewBag.Message.Contains("Bruce"));
+        }
+
+    }
+}
---
+++ HelloApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hello(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                ViewBag.Message = "Hello, please enter your name. Supply it as a GET parameter. (e.g. /Hello?name=Mickey Mouse&)";
+            }
+            else
+            {
+                ViewBag.Message = "Hello " + name;
+            }
+
+
+            return View();
+        }
+    }
+}
